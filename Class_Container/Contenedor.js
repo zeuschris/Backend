8dc0f22a707e877832c9c52bae60f9e483601d55
Chris Montes <zeuschris123@gmail.com>
2022-10-20 14:58:23 -0300

    config fns

--- Class_Container/Contenedor.js
+++ Class_Container/Contenedor.js
@@ -1,111 +1,130 @@
-const fs = require('fs')
+const fs= require('fs');
 
-class Contenedor {
+class Contenedor{
     constructor(archivo){
-        this.archivo = archivo
+        this.archivo=archivo;
+      }
+    
+    save=async(producto)=>{
+       try {
+        //generar el archivo
+        if(fs.existsSync(this.archivo)){
+           let productos= await this.getAll();
+           if( productos.length>0){
+           let lastiId=productos[productos.length-1].id+1
+           let newProduct={
+             id: lastiId,
+            ...producto
+           }
+           productos.push(newProduct);
+            await fs.promises.writeFile(this.archivo,JSON.stringify(productos,null,2))
+            return lastiId;
+        }else{
+            let lastiId=1
+            let newProduct={
+              id: lastiId,
+             ...producto
+            }
+            productos.push(newProduct);
+            await fs.promises.writeFile(this.archivo,JSON.stringify(productos,null,2))
+            return lastiId;
+        }
+    
+        }else{
+        //generamos un producto con id 1 ya que es la creacion del archivo
+        let newProduct={
+        id:1,
+        title: producto.title,
+        price: producto.price,
+        thumbnail: producto.thumbnail
+        //... producto utilizamos el spread operator para copiar la informacion dentro del nuevo producto
+        }
+         await fs.promises.writeFile(this.archivo,JSON.stringify([newProduct],null,2));
+        return 1;
+        }
+        } catch (error) {
+        console.log(error)
+      }
+  }
+
+    async getAll() {
+      try {
+          if(fs.existsSync(this.archivo)){
+          let info= await fs.promises.readFile(this.archivo, 'utf8');
+          let result= JSON.parse(info);
+          return result;
+          }
+          else{
+            return "No se encontro el archivo"
+          }  
+      } catch (error) {
+          console.log(error)
+      }  
     }
 
-    save = async (producto) => {
-        try{
-            if (fs.existsSync(this.archivo)){
-                let info = await fs.promises.readFile(this.archivo, 'utf-8')
-                // Convirtiendo el archivo en array
-                let result = JSON.parse(info)
-                if (result.length > 0){
-                    let lastId = result[result.length-1].id+1
-                    let newProduct = {
-                        id: lastId,
-                        ...producto
-                    }
-                    result.push(newProduct)
-                    await fs.promises.writeFile(this.archivo, JSON.stringify(result, null, 2))
-                    return lastId
-                }else{
-                    // Sobreescribir el array
-                    let lastId = 1
-                    let newProduct = {
-                        id: lastId,
-                        ...producto
-                    }
-                    result.push(newProduct)
-                    await fs.promises.writeFile(this.archivo, JSON.stringify(result, null, 2))
-                    return lastId
-                }
-                }else{
-                    let newProduct = {
-                    id : 1,
-                    ...producto
-                }
-                    await fs.promises.writeFile(this.archivo, JSON.stringify([newProduct], null, 2))
-                    return 1
-            }
-        }catch{(console.log)}
-    }
-}
-
-    getAll = async () => {
-        try {
-            if(fs.existsSync(this.archivo)){
-                let info= await fs.promises.readFile(this.archivo, 'utf8');
-                let result= JSON.parse(info);
-                return result;
-            }else{
-                return "No se encontro el archivo"
-        }  
-    }catch {(console.log)}
-}
-
-    getById = async (id) => {
-        try {
-            // buscar que el archivo exista
-            if(fs.existsSync(this.archivo)){
-            let info= await fs.promises.readFile(this.archivo, 'utf8');
-                const dataId = info.find(item => item.id === id);
-                if (dataId.length === 0) {
-                throw new Error(
-                        `Este producto con el id: ${id} no existe.`
-                    );
-                } else {
-                    console.log(`El producto ${id} fue encontrado:\n`, dataId);
-                    return dataId;
-                }
-            }
-        } catch (error) {console.log(`Error buscando el producto con el id: ${error.message}`)}
-}
-
-    deleteById = async (id) => {
-        // buscar que el archivo exista
-        try {
+    async getById(id) {
+      try {
+          // chequeo que exista el documento
           if(fs.existsSync(this.archivo)){
             let info= await fs.promises.readFile(this.archivo, 'utf8');
-                // verificar si existe el id
-                console.log(`Buscando el producto ${id}`);
-                if (info.some(item => item.id === id)) {
-                    const data = await this.readFile(this.archivo);
-                    // eliminar producto
-                    console.log(`Eliminando el producto ${id}...`);
-                    const datos = data.filter(item => item.id !== id);
-                    this.writeFile(this.archivo, datos);
-                    console.log(`El producto ${id} ha sido eliminado`);
-                } else {
-                    throw new Error(
-                        `No se encontro el producto con el id ${id}`
+          // uso filter para buscar el producto con el id que queramos
+              const dataId = info.filter(item => item.id === id);
+              if (dataId.length === 0) {
+              throw new Error(
+                      "No se encontro un producto con el id solicitado"
+                  );
+              } else {
+                  console.log(`Producto con id ${id} encontrado:\n`, dataId);
+                  return dataId;
+              }
+          }
+      } catch (error) {
+          console.log(`Error buscando producto con el id: ${error.message}`);
+      }
+  }
+
+  async deleteById(id) {
+    // chequeo si existe el documento
+    try {
+      if(fs.existsSync(this.archivo)){
+        let info= await fs.promises.readFile(this.archivo, 'utf8');
+            // verifico que exista el id 
+            console.log(`Buscando producto con el id solicitado...`);
+            if (info.some(item => item.id === id)) {
+                const data = await this.readFile(this.archivo);
+                // elimino producto
+                console.log(`Eliminando producto con id solicitado...`);
+                const datos = data.filter(item => item.id !== id);
+                this.writeFile(this.archivo, datos);
+                console.log(`Producto con el id ${id} eliminado`);
+            } else {
+                throw new Error(
+                    `No se encontro el producto con el id ${id}`
                 );
             }
         }
-    } catch{console.log(`Ocurrio un error eliminando el producto`)}
-}
-    
-    deleteAll = async () => {
-        try {
-            // buscar que el archivo exista
-            let nuevoArray = [];
-            console.log('Borrando productos');
-            await this.writeFile(this.archivo, nuevoArray);
-            console.log(`Se borraron todos los productos del archivo ${this.archivo}`);
-    } catch{console.log('Ha ocurrido un error eliminando los productos')}
+    } catch (error) {
+        console.log(
+            `Ocurrio un error eliminando el producto con el id solicitado: ${error.message}`
+        );
+    }
 }
 
-  
+async deleteAll() {
+    try {
+        // chequeo si existe el documento
+        let nuevoArray = [];
+        console.log(`Borrando datos...`);
+        await this.writeFile(this.archivo, nuevoArray);
+        console.log(
+            `Se borraron todos los datos del archivo ${this.archivo}`
+        );
+    } catch (error) {
+        console.log(
+            `Ocurrio un error eliminando los datos: ${error.message}`
+        );
+    }
+}
+}
 
-module.exports = Contenedor;
+module.exports = Contenedor
--- Class_Container/main.js
+++ Class_Container/main.js
@@ -1,20 +1,20 @@
 const Contenedor = require("./contenedor");
 
-let contenedor = new Contenedor("productos.txt")
+let containerProducts = new Contenedor("./productos.json")
 
-const producto1 = {
+let producto1 = {
     title: "boligrafo parker",
     price: 7500,
     thumbnail: "https://medias.musimundo.com/medias/00476007-144936-144936-01-144936-01.jpg"
 }
 
-const producto2 = {
+let producto2 = {
     title: "mochila jeansport",
     price: 10200,
     thumbnail: "https://medias.musimundo.com/medias/00558060-146480-146480-01-146480-01.jpg"
 }
 
-const producto3 = {
+let producto3 = {
     title: "cuadro de audrey",
     price: 20500,
     thumbnail: "https://medias.musimundo.com/medias/00199039-135728-135728-01-135728-01.jpg"
@@ -22,10 +22,9 @@ const producto3 = {
 
 fns = async () => {
 
-    console.log( await contenedor.save(producto1))
-    console.log( await contenedor.save(producto2))
-    console.log( await contenedor.save(producto3))
-
+    console.log( await containerProducts.save(producto1))
+    console.log( await containerProducts.save(producto2))
+    console.log( await containerProducts.save(producto3))
 }
 
 fns()
