8773fcab12c0992800170a9a05f018b199660520
Chris Montes <zeuschris123@gmail.com>
2022-11-09 11:44:16 -0300

    methods

--- ApiRest/js/main.js
+++ ApiRest/js/main.js
@@ -2,7 +2,6 @@ const express = require('express')
 const {Router} = express
 const app = express()
 const PORT = 8080
-
 const pathProducts = Router()
 
 app.use(express.json())
@@ -53,33 +52,34 @@ class Container {
 const productos = new Container ([]);
 
 productos.save({
-  title: 'Monitor Gamer27',
+  title: 'Monitor LG Gamer27',
   price: '40000',
-  marca : 'LG',
   thumbnail: 'https://www.sintagmatecnologia.com.ar/Temp/App_WebSite/App_PictureFiles/Items/8806091486851_800.jpg'
 });
 
 productos.save({
-  title: 'Silla Gamer',
+  title: 'Silla Gamer Daewoo',
   price: '23000',
-  marca : 'Daewoo',
   thumbnail: 'https://www.lavoz.com.ar/resizer/dqVc7U87qN7ExIT1Mp1ECjB4khM=/1200x793/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/JEQFOFZHXVAG7HGFJOVJN4MBSM.jpg'
 });
 
 productos.save({
-  title: 'Teclado Mecanico',
+  title: 'Teclado HyperX',
   price: '9500',
-  marca : 'HyperX',
   thumbnail: 'https://media.spdigital.cl/__sized__/products/pq2vqokh_880fa69c-thumbnail-1080x1080-70.jpg'
 });
 
 productos.save({
-  title: 'Mouse Gamer',
+  title: 'Mouse Redragon',
   price: '6900',
-  marca : 'Redragon',
   thumbnail: 'https://mexx-img-2019.s3.amazonaws.com/37396_4.jpeg'
 });
 
+
+pathProducts.get('/', (req,res) => {
+    const listProducts = productos.getAll()
+    res.json(listProducts)
+})
 
 pathProducts.get('/:id', (req, res) => {
     const id = Number(req.params.id)
@@ -92,23 +92,26 @@ pathProducts.get('/:id', (req, res) => {
     }
 })
 
-pathProducts.get('/', (req,res) => {
-    const listProducts = productos.getAll()
-    res.json(listProducts)
-})
-
 pathProducts.post('/', (req,res) => {
-    res.send('ok')
-    const producto = req.body
-    this.productos.push(producto)
+    const producto = req.body;
+    productos.save(producto);
+    res.send("ok");
+});
+
+pathProducts.put('/:id', (req,res) => {
+    const update = req.body
+    const id = req.params.id
+    const index = productos.findIndex(producto => producto.id === id)
+
+    if(index >= 0) {
+        productos[index] = update
+    }
+    res.send(productos)
 })
 
-pathProducts.put('/', (req,res) => {
-    res.send('ok')
-})
-
-pathProducts.delete('/', (req,res) => {
-    res.send('ok')
+pathProducts.delete('/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    productos.deleteById(id)
 })
 
 app.use('/api/productos', pathProducts)
